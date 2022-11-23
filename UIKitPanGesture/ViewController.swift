33b5803732aae263edbbddb4dcc594f5b30080c3
Olli Toivanen <olliteed@gmail.com>
2022-11-23 11:02:35 +0200

    Initial project status

---
+++ UIKitPanGesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPanGesture
+//
+//  Created by Olli Toivanen on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
